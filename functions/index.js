60d2fb7775c95012ef149c95acd4cf45797ffec2
srikaanthtb <srikaanth.t.b@gmail.com>
2022-06-19 10:59:40 +0100

    logic complete

--- functions/index.js
+++ functions/index.js
@@ -70,6 +70,7 @@ exports.getRichQuick = functions
     console.log(`Thanks for the tips Jim! ${stocksToBuy}`);
 
     if (!stocksToBuy) {
+      // sell logic if cramer recommends buying a stock you hold
       // console.log('sitting this one out');
       // return null;
       const gptCompletion = await openai.createCompletion('text-davinci-001', {
@@ -82,6 +83,12 @@ exports.getRichQuick = functions
       });
       const stocksToSell = gptCompletion.data.choices[0].text.match(/\b[A-Z]+\b/g);
       console.log(`Thanks for the tips Jim! ${stocksToSell}`);
+      // if a match is found sell it (assuming 1 position is held)
+      const currentPosition = await alpaca.getPositions()
+      if (currentPosition.includes(stocksToSell)) {
+        console.log(`this is being sold: ${stocksToSell}`)
+        const yeet = await alpaca.closeAllPositions();
+      }
     }
 
     //// ALPACA Make Trades ////
