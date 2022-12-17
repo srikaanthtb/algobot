7c865e4877a96ed8d6e874666cc3cfd1f9456883
srikaanthtb <srikaanth.t.b@gmail.com>
2022-12-17 16:16:19 +0000

    openai model update

--- functions/index.js
+++ functions/index.js
@@ -124,7 +124,7 @@ exports.getRichQuick = functions
     const tweets = await scrape();
 
     const gptCompletion = await openai.createCompletion({
-      model: "text-davinci-002",
+      model: "text-davinci-003",
       prompt: `${tweets} Jim Cramer recommends selling the following stock tickers: `,
       temperature: 0.7,
       max_tokens: 32,
